79ae96e421f45701d9aa06bd70137101e3c33271
Dhvanil Pansuriya <dhvanilpansuriya2005@gmail.com>
2024-01-29 22:26:54 +0530

    All Setup Configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// Router Import
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@
  */
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => {
       console.log("Error occurred in asyncHandler : ", err);
     });
